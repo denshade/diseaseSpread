943a9a7690aa1570549bc6e4b79b65e9c71a3799
denshade <lieven.vaneeckhaute@gmail.com>
2020-10-25 12:26:21 +0100

    days infectious

--- js/disease.js
+++ js/disease.js
@@ -1,10 +1,25 @@
 var populationOfPersons = [];
 var Person = /** @class */ (function () {
-    function Person(isInfected) {
-        this.isInfected = isInfected;
+    function Person(isInfected, daysUntilHealed) {
+        if (isInfected) {
+            this.tryInfect(daysUntilHealed);
+        }
+        this.isImmune = false;
     }
-    Person.prototype.tryInfect = function () {
-        this.isInfected = true;
+    Person.prototype.tryInfect = function (daysInfectious) {
+        if (!this.isImmune && !this.isInfected) {
+            this.isInfected = true;
+            this.daysUntilHealed = daysInfectious;
+        }
+    };
+    Person.prototype.tryHeal = function () {
+        if (this.isInfected) {
+            this.daysUntilHealed--;
+            if (this.daysUntilHealed < 1) {
+                this.isInfected = false;
+                this.isImmune = true;
+            }
+        }
     };
     Person.prototype.toString = function () {
         return this.isInfected + "";
@@ -17,64 +32,74 @@ var render = function (canvas, persons) {
     canvas.width = squareSize;
     canvas.height = squareSize;
     var ctx = canvas.getContext('2d');
-    var myImageData = ctx.createImageData(squareSize, squareSize);
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = "#FFF000";
     ctx.fillRect(0, 0, squareSize, squareSize);
     //433
     for (var k = 0; k < persons.length; k++) {
         if (persons[k].isInfected) {
             ctx.fillStyle = "red";
         }
+        else if (persons[k].isImmune) {
+            ctx.fillStyle = "blue";
+        }
         else {
             ctx.fillStyle = "green";
         }
         ctx.fillRect(k % squareSize, k / squareSize, 1, 1);
     }
-    //460
-    /*    persons.forEach((value, index)=> {
-          if (value.isInfected) {
-                ctx.fillStyle = "red";
-            } else {
-                ctx.fillStyle = "green";
-            }
-            ctx.fillRect(index % squareSize,index / squareSize,1, 1);
-        })*/
     var end = new Date().getTime();
     console.log("Performance " + (end - start));
 };
-var initializePopulation = function (populationOfPersons, numberPeople, infectedSize) {
+var initializePopulation = function (populationOfPersons, numberPeople, infectedSize, daysUntilHealed) {
     populationOfPersons.length = 0;
     for (var i = 0; i < infectedSize; i++) {
-        populationOfPersons.push(new Person(true));
+        populationOfPersons.push(new Person(true, daysUntilHealed));
     }
     while (populationOfPersons.length < numberPeople) {
-        populationOfPersons.push(new Person(false));
+        populationOfPersons.push(new Person(false, daysUntilHealed));
     }
     return populationOfPersons;
 };
-var infectOthers = function (infectOtherCount) {
-    var infected = populationOfPersons.filter(function (s) { return s.isInfected; });
+var infectOthers = function (infectOtherCount, daysInfectious) {
+    var infected = populationOfPersons.filter(function (s) { return s.isInfected === true; });
+    for (var k = 0; k < infected.length; k++) {
+        var p = infected[k];
+        p.tryHeal();
+    }
     for (var k = 0; k < infected.length; k++) {
         for (var z = 0; z < infectOtherCount; z++) {
             var index = Math.round(Math.random() * (populationOfPersons.length - 1));
             var p = populationOfPersons[index];
-            p.tryInfect();
+            p.tryInfect(daysInfectious);
         }
     }
 };
 var init = function () {
+    var initbtn = document.getElementById('initbtn');
+    initbtn.disabled = true;
     var canvas = document.getElementById('populationCanvas');
     var populationString = document.getElementById('population').value;
     var populationSize = parseInt(populationString);
     var infectedString = document.getElementById('numberInfected').value;
     var infectedSize = parseInt(infectedString);
-    initializePopulation(populationOfPersons, populationSize, infectedSize);
+    var daysInfectiousStr = document.getElementById('daysInfectious').value;
+    var daysInfectious = parseInt(daysInfectiousStr);
+    initializePopulation(populationOfPersons, populationSize, infectedSize, daysInfectious);
     render(canvas, populationOfPersons);
+    initbtn.disabled = false;
 };
 var simulate = function () {
+    var initbtn = document.getElementById('initbtn');
+    var stepbtn = document.getElementById('stepbtn');
+    initbtn.disabled = true;
+    stepbtn.disabled = true;
     var canvas = document.getElementById('populationCanvas');
     var infectOtherCountStr = document.getElementById('infectOtherCount').value;
     var infectOtherCount = parseInt(infectOtherCountStr);
-    infectOthers(infectOtherCount);
+    var daysInfectiousStr = document.getElementById('daysInfectious').value;
+    var daysInfectious = parseInt(daysInfectiousStr);
+    infectOthers(infectOtherCount, daysInfectious);
     render(canvas, populationOfPersons);
+    initbtn.disabled = false;
+    stepbtn.disabled = false;
 };
--- js/disease.ts
+++ js/disease.ts
@@ -3,15 +3,36 @@ const populationOfPersons = [];
 class Person
 {
     isInfected : boolean;
+    daysUntilHealed : number;
+    isImmune: boolean;
 
-    constructor(isInfected:boolean)
+    constructor(isInfected:boolean, daysUntilHealed: number)
     {
-        this.isInfected = isInfected;
+        if (isInfected) {
+            this.tryInfect(daysUntilHealed);
+        }
+        this.isImmune = false;
     }
 
-    public tryInfect()
+    public tryInfect(daysInfectious: number)
     {
-        this.isInfected = true;
+        if (!this.isImmune && !this.isInfected)
+        {
+            this.isInfected = true;
+            this.daysUntilHealed = daysInfectious;
+        }
+    }
+
+    public tryHeal()
+    {
+        if (this.isInfected)
+        {
+            this.daysUntilHealed --;
+            if (this.daysUntilHealed < 1) {
+                this.isInfected = false;
+                this.isImmune = true;
+            }
+        }
     }
 
     public toString() : string
@@ -28,73 +49,91 @@ const render = (canvas: HTMLCanvasElement, persons : Person[]) =>
     canvas.width = squareSize;
     canvas.height = squareSize;
     const ctx = <CanvasRenderingContext2D>canvas.getContext('2d');
-    var myImageData = ctx.createImageData(squareSize, squareSize);
-
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = "#FFF000";
     ctx.fillRect(0, 0, squareSize, squareSize);
+
     //433
     for (let k = 0; k < persons.length; k++ ) {
         if (persons[k].isInfected) {
             ctx.fillStyle = "red";
+        } else if (persons[k].isImmune) {
+            ctx.fillStyle = "blue";
         } else {
             ctx.fillStyle = "green";
         }
         ctx.fillRect(k % squareSize,k / squareSize,1, 1);
     }
-    //460
-/*    persons.forEach((value, index)=> {
-      if (value.isInfected) {
-            ctx.fillStyle = "red";
-        } else {
-            ctx.fillStyle = "green";
-        }
-        ctx.fillRect(index % squareSize,index / squareSize,1, 1);
-    })*/
+
     var end = new Date().getTime();
     console.log("Performance " + (end-start));
 }
 
-const initializePopulation = (populationOfPersons: Person[], numberPeople : number, infectedSize : number) : Person[] =>
+const initializePopulation = (populationOfPersons: Person[], numberPeople : number, infectedSize : number, daysUntilHealed : number) : Person[] =>
 {
     populationOfPersons.length = 0;
     for (let i = 0; i < infectedSize; i++)
     {
-        populationOfPersons.push(new Person(true));
+        populationOfPersons.push(new Person(true, daysUntilHealed));
     }
     while(populationOfPersons.length < numberPeople)
     {
-        populationOfPersons.push(new Person(false));
+        populationOfPersons.push(new Person(false, daysUntilHealed));
     }
     return populationOfPersons;
 }
 
-const infectOthers = (infectOtherCount) => {
-    const infected = populationOfPersons.filter(s => s.isInfected);
+const infectOthers = (infectOtherCount : number, daysInfectious: number) => {
+    const infected = populationOfPersons.filter(s => s.isInfected === true);
+
+    for (let k = 0; k < infected.length; k++)
+    {
+        let p: Person = infected[k];
+        p.tryHeal();
+    }
+
     for (let k = 0; k < infected.length; k++)
     {
         for (let z = 0; z < infectOtherCount; z++)
         {
             const index = Math.round(Math.random() * (populationOfPersons.length - 1));
             let p: Person = populationOfPersons[index];
-            p.tryInfect();
+            p.tryInfect(daysInfectious);
         }
     }
 }
 
 const init = () => {
+    const initbtn = document.getElementById('initbtn')  as HTMLInputElement;
+    initbtn.disabled = true;
+
     const canvas = <HTMLCanvasElement>document.getElementById('populationCanvas');
     const populationString = (<HTMLInputElement>document.getElementById('population')).value;
     const populationSize : number = parseInt(populationString);
     const infectedString = (<HTMLInputElement>document.getElementById('numberInfected')).value;
     const infectedSize : number = parseInt(infectedString);
-    initializePopulation(populationOfPersons, populationSize, infectedSize);
+    const daysInfectiousStr = (<HTMLInputElement>document.getElementById('daysInfectious')).value;
+    const daysInfectious : number = parseInt(daysInfectiousStr);
+
+    initializePopulation(populationOfPersons, populationSize, infectedSize, daysInfectious);
     render(canvas, populationOfPersons)
+    initbtn.disabled = false;
 }
 
 const simulate = () => {
+
+    const initbtn = document.getElementById('initbtn')  as HTMLInputElement;
+    const stepbtn = document.getElementById('stepbtn')  as HTMLInputElement;
+
+    initbtn.disabled = true;
+    stepbtn.disabled = true;
+
     const canvas = <HTMLCanvasElement>document.getElementById('populationCanvas');
     const infectOtherCountStr = (<HTMLInputElement>document.getElementById('infectOtherCount')).value;
     const infectOtherCount : number = parseInt(infectOtherCountStr);
-    infectOthers(infectOtherCount);
+    const daysInfectiousStr = (<HTMLInputElement>document.getElementById('daysInfectious')).value;
+    const daysInfectious : number = parseInt(daysInfectiousStr);
+    infectOthers(infectOtherCount, daysInfectious);
     render(canvas, populationOfPersons);
+    initbtn.disabled = false;
+    stepbtn.disabled = false;
 }
